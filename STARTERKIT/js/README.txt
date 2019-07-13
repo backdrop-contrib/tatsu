94c7d13e32df4e0348f56aff95cfa130ea73aca3
stpaultim <tim@triplo.co>
2019-07-13 18:07:40 -0500

    Issue #6: Add SASS Support - Just getting started

---
+++ STARTERKIT/js/README.txt
@@ -0,0 +1,3 @@
+Use this file to add JS to your sub-theme.
+
+#ThingsToDo = Add better instructions for a beginner themer
---
+++ STARTERKIT/theme-settings.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @file
+ * Theme settings file for Basis.
+ *
+ * Although Basis itself does not provide any settings, we use this file to
+ * inform the user that the module supports color schemes if the Color module
+ * is enabled.
+ */
+if (module_exists('color')) {
+  $form['header'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Header Settings'),
+    '#collapsible' => TRUE,
+  );
+  $fields = array(
+    'header',
+    'slogan',
+    'titleslogan',
+    'hovermenu',
+    'menutoggle',
+  );
+  foreach ($fields as $field) {
+    $form['header'][$field] = color_get_color_element('tatsu', $field, $form);
+  }
+
+  $form['general'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('General Settings'),
+    '#collapsible' => TRUE,
+  );
+  $fields = array(
+    'bg',
+    'text',
+    'link',
+    'borders',
+    'formfocusborder',
+  );
+  foreach ($fields as $field) {
+    $form['general'][$field] = color_get_color_element('tatsu', $field, $form);
+  }
+
+  $form['hero_block'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Hero Blocks'),
+    '#collapsible' => TRUE,
+  );
+  $fields = array(
+    'heroone',
+    'herotwo',
+    'herothree',
+    'herofour',
+    'herotext',
+  );
+  foreach ($fields as $field) {
+    $form['hero_block'][$field] = color_get_color_element('tatsu', $field, $form);
+  }
+
+  $form['primary_tabs'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Tabs and Breadcrumb'),
+    '#collapsible' => TRUE,
+  );
+  $fields = array(
+    'primarytabs',
+    'primarytabstext',
+    'buttons',
+  );
+  foreach ($fields as $field) {
+    $form['primary_tabs'][$field] = color_get_color_element('tatsu', $field, $form);
+  }
+
+  $form['footer'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Footer Settings'),
+    '#collapsible' => TRUE,
+  );
+  $fields = array(
+    'footerborder',
+    'footer',
+    'footertext',
+  );
+  foreach ($fields as $field) {
+    $form['footer'][$field] = color_get_color_element('tatsu', $field, $form);
+  }
+}
+else {
+  $form['color'] = array(
+    '#markup' => '<p>' . t('This theme supports custom color palettes if the Color module is enabled on the <a href="!url">modules page</a>. Enable the Color module to customize this theme.', array('!url' => url('admin/modules'))) . '</p>',
+  );
+}
